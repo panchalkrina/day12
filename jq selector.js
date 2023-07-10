b38d623cfba1d035608c59107cdc63b7bcd56beb
panchalkrina <137513535+panchalkrina@users.noreply.github.com>
2023-07-10 21:49:58 +0530

    Add files via upload

---
+++ jq selector.js
@@ -0,0 +1,6 @@
+function funt1()
+{
+    $("#div2").css("background-color","blue");
+    $("div").css("background-color","green");
+    $("#p1").css('font-style','ital');
+}
---
+++ jq.js
@@ -0,0 +1,5 @@
+function funct1()
+{
+ // $("#div2").css("background-color","blue");  
+ $("p:second").fadeToggle();
+}
